16d483956b97dc95d8b1eeb7b0ac630e48219820
NOPP-PH <nopphakorn.ph@gmail.com>
2023-07-16 21:40:50 +0700

    install react & set first page

---
+++ crud-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import NavBar from './NavBar.js';
+import User from './User.js';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
